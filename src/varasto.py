3a353969467072e15414577afad34ae10e14f0bf
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-11-14 11:27:14 +0200

    Viikko 2, tehtävä 3: Tilanne, jossa kaikki pylint-säännöt täyttyvät

--- src/varasto.py
+++ src/varasto.py
@@ -1,28 +1,19 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(tilavuus, 0.0)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
-            if self.saldo == 0.0:
-                print("Hello")
-                if self.saldo != 1.0:
-                    print("Hello")
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
         else:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
-            
-        print("Hello, world")
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
